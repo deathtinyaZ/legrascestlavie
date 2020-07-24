2297381761835357b53cb12700b62078c45b1896
deathtinyaZ <68634598+deathtinyaZ@users.noreply.github.com>
2020-07-24 14:04:42 +0200

    Add files via upload

---
+++ inscription/inscription.js
@@ -0,0 +1,1 @@
+<button OnClick=" ./presentation.html ">registerNew</button>
---
+++ presentation/presentation.js
@@ -0,0 +1,4 @@
+<button OnClick=" ./inscription.html ">S'enregistrer</button>
+<button OnClick=" ./connexion.html ">Se connecter</button>
+<button OnClick=" ./recherche.html ">recherche</button>
+<button OnClick=" ./adminpage.html ">adminpage</button>
